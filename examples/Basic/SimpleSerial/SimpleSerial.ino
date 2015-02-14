ae69575ff39e416aec4a51bb2bce0cf4342b908b
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-02-15 00:47:45 +0200

    Fixed linux script

--- examples/Basic/SimpleSerial/SimpleSerial.ino
+++ examples/Basic/SimpleSerial/SimpleSerial.ino
@@ -1,10 +1,9 @@
 #include <BlynkSimpleSerial.h>
 
 // Connect Arduino to your computer with USB cable.
-// Run an app to relay Arduino <-> server connection.
-//    Windows:       blynk-ser.bat
-//    Linux and OSX: blynk-ser.sh
-// Edit the script to specify serial port of Arduino and run it
+// Specify serial port of Arduino in the script and run it:
+//    Windows:         blynk-ser.bat
+//    Linux and OSX:   blynk-ser.sh
 
 // Auth token you get from App
 char auth[] = "6b291640fd1c4bf69368bf555f514bb9";
--- examples/Basic/SimpleSerial/blynk-ser.sh
+++ examples/Basic/SimpleSerial/blynk-ser.sh
@@ -1,4 +1,4 @@
-!#/bin/sh
+#!/bin/sh
 
 # === Edit these lines to match your need ===
 COMM_PORT=/dev/ttyACM0
@@ -8,6 +8,6 @@ SERV_PORT=8181
 
 # === Edit lines below only if absolutely sure what you're doing ===
 while [ 1 ]; do
-  socat -d -d FILE:$COMM_PORT,raw,echo=0,b$COMM_BAUD,nonblock=1 TCP:$SERV_ADDR:$SERV_PORT
+  socat -d FILE:$COMM_PORT,raw,echo=0,b$COMM_BAUD,nonblock=1 TCP:$SERV_ADDR:$SERV_PORT
   sleep 2
 done
