0820d70d90de959d047152ecb14f73548de65f9e
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-03-05 22:27:02 +0200

    Combine C++ files for Arduino IDE

---
+++ BlynkArduino.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file       BlynkParam.h
+ * @author     Volodymyr Shymanskyy
+ * @date       Mar 2015
+ * @brief      This file includes other C++ files to be built in Arduino IDE
+ *
+ */
+
+#include <Blynk/BlynkParam.cpp>
+#include <Blynk/BlynkHandlers.cpp>
