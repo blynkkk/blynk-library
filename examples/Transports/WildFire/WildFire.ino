e5141b2c0dfa67cf5c673e8659fb2c23ca49647d
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-03-18 14:37:38 +0200

    Update WildFire example

--- examples/Transports/WildFire/WildFire.ino
+++ examples/Transports/WildFire/WildFire.ino
@@ -3,6 +3,8 @@
  * and also to install "Combined Arduino Support Package".
  * Please find more information here:
  *   http://shop.wickeddevice.com/resources/wildfire
+ *
+ * Change WiFi ssid, pass, and Blynk auth token to run :)
  */
 
 #define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
@@ -10,6 +12,9 @@
 #include <ccspi.h>
 #include <SPI.h>
 #include <BlynkSimpleWildFire.h>
+#include <WildFire.h>
+
+WildFire wildfire;
 
 // Auth token you get from App
 char auth[] = "00000000000000000000000000000000";
@@ -17,7 +22,9 @@ char auth[] = "00000000000000000000000000000000";
 void setup()
 {
   Serial.begin(9600);
+  wildfire.begin();
   Blynk.begin(auth, "ssid", "pass", WLAN_SEC_WPA2);
+  timer.setInterval(1000, sendUptime);
 }
 
 void loop()
