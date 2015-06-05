494634eeda22d0efd91048c1626b9d04acad39d8
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-06-05 09:26:33 +0300

    Added Seeed Ethernet W5200 support (Compilation tested only).

---
+++ examples/BoardsAndShields/Arduino_EthernetV2_0/Arduino_EthernetV2_0.ino
@@ -0,0 +1,60 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social groups:              http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ *
+ * This example shows how to use Seeed Ethernet Shield V2.0 (W5200)
+ * to connect your project to Blynk.
+ *
+ * More about the shield (and library download):
+ *   http://www.seeedstudio.com/wiki/Ethernet_Shield_V2.0
+ * (It looks like this library for W5200 works only on Arduino IDE 1.0.*)
+ *
+ * Note: This Hardware is not tested.
+ *
+ * Feel free to apply it to any other example. It's simple!
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <SPI.h>
+#include <EthernetV2_0.h>
+#include <BlynkSimpleEthernetV2_0.h>
+
+#define W5200_CS  10
+#define SDCARD_CS 4
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(SDCARD_CS,OUTPUT);
+  digitalWrite(SDCARD_CS,HIGH);//Deselect the SD card
+
+  Blynk.begin(auth);
+  // You can also specify server.
+  // For more options, see Transports/Advanced/CustomEthernet example
+  //Blynk.begin(auth, "server.org", 8442);
+  //Blynk.begin(auth, IPAddress(192,168,1,100), 8888);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
