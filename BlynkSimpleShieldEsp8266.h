87903116b80241a5f8ee827ba8e69cc3e5152aea
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-06-10 12:07:12 +0300

    ESP8266 Shield support

---
+++ BlynkSimpleShieldEsp8266.h
@@ -0,0 +1,153 @@
+/**
+ * @file       BlynkSimpleShieldEsp8266.h
+ * @author     Volodymyr Shymanskyy
+ * @license    This project is released under the MIT License (MIT)
+ * @copyright  Copyright (c) 2015 Volodymyr Shymanskyy
+ * @date       Jun 2015
+ * @brief
+ *
+ */
+
+#ifndef BlynkSimpleShieldEsp8266_h
+#define BlynkSimpleShieldEsp8266_h
+
+
+#include <BlynkApiArduino.h>
+#include <Blynk/BlynkProtocol.h>
+#include <utility/BlynkFifo.h>
+#include <ESP8266.h>
+
+class BlynkTransportShieldEsp8266
+{
+    static void onData(uint8_t mux_id, uint32_t len, void* ptr) {
+        ((BlynkTransportShieldEsp8266*)ptr)->onData(mux_id, len);
+    }
+
+    void onData(uint8_t mux_id, uint32_t len) {
+        while (len) {
+            if (client->getUart()->available()) {
+                uint8_t b = client->getUart()->read();
+                if(!buffer.push(b)) {
+                    BLYNK_LOG("Buffer overflow");
+                }
+                len--;
+            }
+        }
+    }
+
+public:
+    BlynkTransportShieldEsp8266()
+        : client(NULL)
+        , status(false)
+        , domain(NULL)
+        , port(0)
+    {}
+
+    void begin_domain(ESP8266* esp8266, const char* d,  uint16_t p) {
+        client = esp8266;
+        client->setOnData(onData, this);
+        domain = d;
+        port = p;
+    }
+
+    bool connect() {
+        BLYNK_LOG_FN();
+        if (!domain || !port)
+            return false;
+        status = client->createTCP(domain, port);
+        return status;
+    }
+
+    void disconnect() {
+        BLYNK_LOG_FN();
+        status = false;
+        client->releaseTCP();
+    }
+
+    size_t read(void* buf, size_t len) {
+        BLYNK_LOG_FN();
+        while (buffer.getOccupied() < len) {
+            client->run();
+        }
+        return buffer.read((uint8_t*)buf, len);
+    }
+    size_t write(const void* buf, size_t len) {
+        BLYNK_LOG_FN();
+        if (client->send((const uint8_t*)buf, len)) {
+            return len;
+        }
+        return 0;
+    }
+
+    bool connected() { return status; }
+
+    int available() {
+        client->run();
+        return buffer.getOccupied();
+    }
+
+private:
+    ESP8266* client;
+    bool status;
+    BlynkFifo<uint8_t,128> buffer;
+    const char* domain;
+    uint16_t    port;
+};
+
+class BlynkWifi
+    : public BlynkProtocol<BlynkTransportShieldEsp8266>
+{
+    typedef BlynkProtocol<BlynkTransportShieldEsp8266> Base;
+public:
+    BlynkWifi(BlynkTransportShieldEsp8266& transp)
+        : Base(transp)
+        , wifi(NULL)
+    {}
+
+    bool wifi_conn(const char* ssid, const char* pass)
+    {
+        BLYNK_LOG("Connecting to %s", ssid);
+        if (!wifi->setEcho(0)) {
+            BLYNK_LOG("Failed to disable Echo");
+            return false;
+        }
+        if (!wifi->setOprToStation()) {
+            BLYNK_LOG("Failed to set STA mode");
+            return false;
+        }
+        if (wifi->joinAP(ssid, pass)) {
+            BLYNK_LOG("IP: %s", wifi->getLocalIP().c_str());
+        } else {
+            BLYNK_LOG("Failed to connect WiFi");
+            return false;
+        }
+        if (!wifi->disableMUX()) {
+            BLYNK_LOG("Failed to disable MUX");
+        }
+        BLYNK_LOG("Connected to WiFi");
+        return true;
+    }
+
+    void begin(const char* auth,
+               ESP8266&    esp8266,
+               const char* ssid,
+               const char* pass,
+               const char* domain = BLYNK_DEFAULT_DOMAIN,
+               uint16_t    port   = BLYNK_DEFAULT_PORT)
+    {
+        Base::begin(auth);
+        wifi = &esp8266;
+        wifi_conn(ssid, pass);
+        this->conn.begin_domain(wifi, domain, port);
+    }
+
+private:
+    ESP8266* wifi;
+};
+
+static BlynkTransportShieldEsp8266 _blynkTransport;
+BlynkWifi Blynk(_blynkTransport);
+
+#include <BlynkWidgets.h>
+
+#endif
---
+++ examples/BoardsAndShields/ESP8266_Shield_SwSerial/ESP8266_Shield_SwSerial.ino
@@ -0,0 +1,56 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social groups:              http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ *
+ * This example shows how to use ESP8266 Shield
+ * via SoftwareSerial to connect your project to Blynk.
+ *
+ * For this example you need a modified ITEAD WeeESP8266 library:
+ *   https://github.com/vshymanskyy/ITEADLIB_Arduino_WeeESP8266
+ *
+ * Please adjust the settings in the ESP8266.h:
+ *   Set ESP8266_USE_SOFTWARE_SERIAL, if needed
+ *   Set USER_SEL_VERSION to the version of your module
+ *
+ * Note: Ensure a stable serial connection to ESP8266!
+ *
+ * Change WiFi ssid, pass, and Blynk auth token to run :)
+ * Feel free to apply it to any other example. It's simple!
+ *
+ **************************************************************/
+//#define BLYNK_DEBUG
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include "SoftwareSerial.h"
+#include "ESP8266.h"
+#include <BlynkSimpleShieldEsp8266.h>
+
+SoftwareSerial SwSerial(2, 3); // RX, TX
+ESP8266 wifi(SwSerial, 9600);  // Set ESP8266 baud rate
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, wifi, "ssid", "pass");
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
