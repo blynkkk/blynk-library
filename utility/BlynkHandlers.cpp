fc3cdb4d79d68824fe7990700c919182bd00cbba
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-06-26 00:38:40 +0300

    Fix comment

--- utility/BlynkHandlers.cpp
+++ utility/BlynkHandlers.cpp
@@ -4,7 +4,7 @@
  * @license    This project is released under the MIT License (MIT)
  * @copyright  Copyright (c) 2015 Volodymyr Shymanskyy
  * @date       Jan 2015
- * @brief      Debug utilities for Arduino
+ * @brief      Virtual pin utilities
  */
 
 #include <Blynk/BlynkConfig.h>
