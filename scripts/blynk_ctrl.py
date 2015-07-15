a58f0c4ad48285f82656ed42652fd4688a569750
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-07-15 22:56:03 +0300

    Fixed logics [skip ci]

--- scripts/blynk_ctrl.py
+++ scripts/blynk_ctrl.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 '''
- Examples:
-   python blynk_ctrl.py --token=b168... write D0 1
-   python blynk_ctrl.py --token=b168... write A0 123
-   python blynk_ctrl.py --token=b168... write V0 "some value"
-   
- Note: read is not supported yet
+examples:
+  python blynk_ctrl.py --token=909fa1...1a9774 digitalWrite 10 1
+  python blynk_ctrl.py --token=909fa1...1a9774 digitalWrite A0 1
+  python blynk_ctrl.py --token=909fa1...1a9774  analogWrite 9 123
+  python blynk_ctrl.py --token=909fa1...1a9774 virtualWrite 5 "some value"
+
+note:
+  read is not supported yet
 
  Author:   Volodymyr Shymanskyy
  License:  The MIT license
@@ -15,19 +17,19 @@ import sys, time
 import argparse
 
 parser = argparse.ArgumentParser(
-    #usage = '%prog [args] <command> <pin> [<value>...]',
+    formatter_class=argparse.RawTextHelpFormatter,
     description = 'This script uses Bridge feature to control another device from the command line.',
     epilog = __doc__
 )
 
 parser.add_argument('-s', '--server', action='store',      dest='server',           help='server address or domain name')
-parser.add_argument('-p', '--port',   action="store",      dest='port', type=int, help='server port')
+parser.add_argument('-p', '--port',   action="store",      dest='port',   type=int, help='server port')
 parser.add_argument('-t', '--token',  action="store",      dest='token',            help='auth token of the controller')
 parser.add_argument('--target',       action="store",      dest='target', metavar="TOKEN", help='auth token of the target device')
 parser.add_argument('--dump',         action="store_true", dest='dump',             help='dump communication')
-parser.add_argument('command',        help='operation to perform')
-parser.add_argument('pin',            help='pin name')
-parser.add_argument('value',          nargs='*', help='value')
+parser.add_argument('command',                                                      help='operation to perform')
+parser.add_argument('pin',                                                          help='pin name')
+parser.add_argument('value',          nargs='*',                                    help='value')
 
 parser.set_defaults(
     server='cloud.blynk.cc',
@@ -109,6 +111,7 @@ if args.nodelay:
 # Authenticate
 conn.sendall(hdr.pack(MsgType.LOGIN, 1, len(args.token)))
 conn.sendall(args.token)
+
 data = receive(conn, hdr.size)
 if not data:
     log("Login timeout")
@@ -121,29 +124,23 @@ if MsgType.RSP != 0 or msg_id != 1 or status != MsgStatus.OK:
 
 conn.sendall(bridge(args.bridge, "i", args.target));
 
-pin = args.pin
-
-if args.command == 'write' or args.command == 'set':
+if args.command.endswith('Write'):
     if not args.value:
         parser.error("value not specified!")
-    val = args.value
-    if pin[0] == 'D' or pin[0] == 'd':
-        conn.sendall(bridge(args.bridge, "dw", pin[1:], *val));
-    elif pin[0] == 'A' or pin[0] == 'a':
-        conn.sendall(bridge(args.bridge, "aw", 'A'+pin[1:], *val));
-    elif pin[0] == 'V' or pin[0] == 'v':
-        conn.sendall(bridge(args.bridge, "vw", pin[1:], *val));
-    else:
-        log("Invalid pin format")
-elif args.command == 'read' or args.command == 'get':
-    if pin[0] == 'D' or pin[0] == 'd':
-        conn.sendall(bridge(args.bridge, "dr", pin[1:]));
-    elif pin[0] == 'A' or pin[0] == 'a':
-        conn.sendall(bridge(args.bridge, "ar", 'A'+pin[1:]));
-    elif pin[0] == 'V' or pin[0] == 'v':
-        conn.sendall(bridge(args.bridge, "vr", pin[1:]));
-    else:
-        log("Invalid pin format")
+
+    if args.command == 'digitalWrite':
+        conn.sendall(bridge(args.bridge, "dw", args.pin, *args.value));
+    elif args.command == 'analogWrite':
+        conn.sendall(bridge(args.bridge, "aw", args.pin, *args.value));
+    elif args.command == 'virtualWrite':
+        conn.sendall(bridge(args.bridge, "vw", args.pin, *args.value));
+elif args.command.endswith('Read'):
+    if args.command == 'digitalRead':
+        conn.sendall(bridge(args.bridge, "dr", args.pin));
+    elif args.command == 'analogRead':
+        conn.sendall(bridge(args.bridge, "ar", args.pin));
+    elif args.command == 'virtualRead':
+        conn.sendall(bridge(args.bridge, "vr", args.pin));
 
     while True:
         data = receive(conn, hdr.size)
