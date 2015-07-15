794333ff7dcaf3b7f9882e67def72cb000f9e704
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-07-15 12:38:15 +0300

    Update Email.ino
    [ci skip]

--- examples/Widgets/Email/Email.ino
+++ examples/Widgets/Email/Email.ino
@@ -53,7 +53,7 @@ void setup()
   // Setup email button on pin 2
   pinMode(2, INPUT_PULLUP);
   // Attach pin 2 interrupt to our handler
-  attachInterrupt(0, emailOnButtonPress, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(2), emailOnButtonPress, CHANGE);
 }
 
 void emailUptime()
