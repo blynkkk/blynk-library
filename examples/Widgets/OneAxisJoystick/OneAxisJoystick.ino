7793e89511b667b82c6f44b86e85c36dae61c44e
Dmitriy Dumanskiy <doom369@gmail.com>
2015-07-16 16:51:55 +0300

    Joystick examples added

---
+++ examples/Widgets/OneAxisJoystick/OneAxisJoystick.ino
@@ -0,0 +1,52 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ * You can receive 1 coord for joystick movement within App.
+ *
+ * App dashboard setup:
+ *   One Axis Joystick on V1.
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial
+#include <SPI.h>
+#include <Ethernet.h>
+#include <BlynkSimpleEthernet.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth);
+}
+
+BLYNK_WRITE(1) {
+  int x = param.asInt();
+
+  //do something with x
+
+  //for instance you could output them back to application via terminal
+  //terminal1.print("X = ");
+  //terminal1.println(x);
+  //terminal1.flush();
+}
+
+void loop()
+{
+  Blynk.run();
+}
---
+++ examples/Widgets/TwoAxisJoystick/TwoAxisJoystick.ino
@@ -0,0 +1,56 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ * You can receive x and y coords for joystick movement within App.
+ *
+ * App dashboard setup:
+ *   Two Axis Joystick on V1 in MERGE output mode.
+ *   MERGE mode means device will receive both x and y within 1 message
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial
+#include <SPI.h>
+#include <Ethernet.h>
+#include <BlynkSimpleEthernet.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth);
+}
+
+BLYNK_WRITE(1) {
+  int x = param[0].asInt();
+  int y = param[1].asInt();
+
+  //do something with x and y
+
+  //for instance you could output them back to application via terminal
+  //terminal2.print("X = ");
+  //terminal2.print(x);
+  //terminal2.print("; Y = ");
+  //terminal2.println(y);
+  //terminal2.flush();
+}
+
+void loop()
+{
+  Blynk.run();
+}
