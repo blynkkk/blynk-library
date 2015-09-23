1319eb98e60e86003b1b6a5d84f8e2cb96b1935b
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-09-23 23:02:11 +0300

    Separate libraries for hard and soft serial ESP8266

---
+++ examples/BoardsAndShields/ESP8266_Shield_SoftSer/ESP8266_Shield_SoftSer.ino
@@ -0,0 +1,56 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ *
+ * This example shows how to use ESP8266 Shield via Software Serial
+ * (on Uno, Nano...) to connect your project to Blynk.
+ *
+ * Note: Ensure a stable serial connection to ESP8266!
+ *       Firmware version 1.0.0 (AT v0.22) or later is needed.
+ *
+ * Change WiFi ssid, pass, and Blynk auth token to run :)
+ * Feel free to apply it to any other example. It's simple!
+ *
+ **************************************************************/
+//#define BLYNK_DEBUG
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266_SoftSer.h>
+#include <BlynkSimpleShieldEsp8266_SoftSer.h>
+
+// Set ESP8266 Serial object
+#include <SoftwareSerial.h>
+SoftwareSerial EspSerial(2, 3); // RX, TX
+
+ESP8266 wifi(EspSerial);
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  Serial.begin(9600);     // Set console baud rate
+  delay(10);
+  EspSerial.begin(9600);  // Set ESP8266 baud rate
+  delay(10);
+
+  Blynk.begin(auth, wifi, "ssid", "pass");
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
