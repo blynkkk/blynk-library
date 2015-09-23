914a07bd2f8acddcdea7755d1ed1ba56c1053864
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-09-23 23:19:04 +0300

    Dedicated examples for TI boards

---
+++ BlynkSimpleTI_CC3200_LaunchXL.h
@@ -0,0 +1,26 @@
+/**
+ * @file       BlynkSimpleRBL_CC3200.h
+ * @author     Volodymyr Shymanskyy
+ * @license    This project is released under the MIT License (MIT)
+ * @copyright  Copyright (c) 2015 Volodymyr Shymanskyy
+ * @date       Jan 2015
+ * @brief
+ *
+ */
+
+#ifndef BlynkSimpleRBL_CC3200_h
+#define BlynkSimpleRBL_CC3200_h
+
+#ifndef BLYNK_INFO_DEVICE
+#define BLYNK_INFO_DEVICE  "TI CC3200-LaunchXL"
+#endif
+
+#ifndef BLYNK_INFO_CONNECTION
+#define BLYNK_INFO_CONNECTION  "CC3200"
+#endif
+
+#define BLYNK_SEND_CHUNK 64
+
+#include <BlynkSimpleEnergiaWiFi.h>
+
+#endif
