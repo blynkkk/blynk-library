91d7731ea16e0687be280003d4d8aeacfde9067a
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-12-03 23:44:24 +0200

    Add sync example

---
+++ examples/GettingStarted/Sync/Sync.ino
@@ -0,0 +1,66 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ * You can synchronize
+ *
+ * App dashboard setup:
+ *   Slider widget (0...100) on V0
+ *   Slider widget (0...100) on V2
+ *   Button widget on digital pin (conneced to an LED)
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial
+#include <SPI.h>
+#include <Ethernet.h>
+#include <BlynkSimpleEthernet.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  Serial.begin(9600); // See the connection status in Serial Monitor
+  Blynk.begin(auth);
+}
+
+// Keep this flag not to re-sync on every reconnection
+bool isFirstConnect = true;
+
+// This function will run every time Blynk connection is established
+BLYNK_CONNECTED() {
+  if (isFirstConnect) {
+    Blynk.syncFromServer();
+    isFirstConnect = false;
+  }
+  
+  // You can also update some virtual pin
+  // I'll push uptime, just for this example
+  int value = millis()/1000;
+  Blynk.virtualWrite(V2, value);
+}
+
+BLYNK_WRITE(V0)
+{
+  int value = param.asInt();
+  Blynk.virtualWrite(V2, value);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
