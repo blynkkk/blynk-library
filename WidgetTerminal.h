02b3c1de0a723ffc6f56764b330930be3a80cf4e
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-02-06 13:05:31 +0200

    Fix RedBear DUO build

--- WidgetTerminal.h
+++ WidgetTerminal.h
@@ -17,9 +17,9 @@
 #include <Blynk/BlynkApi.h>
 
 #ifdef BLYNK_USE_PRINT_CLASS
-    #if !(defined(SPARK) || defined(PARTICLE))
+    #if !(defined(SPARK) || defined(PARTICLE) || (PLATFORM_ID==88)) // 88 -> RBL Duo
         // On Particle this is auto-included
-        #include <Print.h> // TODO: RBL Duo
+        #include <Print.h>
     #endif
 #endif
 
