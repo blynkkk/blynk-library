d00593dc903ed0562484648aa28f70cf5145d1b0
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-02-06 22:27:09 +0200

    Add ESP8266 SSL support

---
+++ BlynkSimpleEsp8266_SSL.h
@@ -0,0 +1,136 @@
+/**
+ * @file       BlynkSimpleEsp8266_SSL.h
+ * @author     Volodymyr Shymanskyy
+ * @license    This project is released under the MIT License (MIT)
+ * @copyright  Copyright (c) 2015 Volodymyr Shymanskyy
+ * @date       Jan 2016
+ * @brief
+ *
+ */
+
+#ifndef BlynkSimpleEsp8266_SSL_h
+#define BlynkSimpleEsp8266_SSL_h
+
+#ifndef ESP8266
+#error This code is intended to run on the ESP8266 platform! Please check your Tools->Board setting.
+#endif
+
+#ifndef BLYNK_INFO_DEVICE
+#define BLYNK_INFO_DEVICE  "ESP8266"
+#endif
+
+#define BLYNK_DEFAULT_FINGERPRINT "A2 10 3C 79 7D 19 FB B2 50 0D 02 C6 39 8B 94 D0 02 B8 88 88"
+
+#include <BlynkApiArduino.h>
+#include <Blynk/BlynkProtocol.h>
+#include <Adapters/BlynkArduinoClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+template <typename Client>
+class BlynkArduinoClientSecure
+	: public BlynkArduinoClientGen<Client>
+{
+public:
+	BlynkArduinoClientSecure(Client& client)
+		: BlynkArduinoClientGen<Client>(client)
+		, fingerprint(NULL)
+	{}
+
+	void setFingerprint(const char* fp) { fingerprint = fp; }
+
+    bool connect() {
+        if (BlynkArduinoClientGen<Client>::connect()) {
+		  if (fingerprint && !this->client.verify(fingerprint, this->domain)) {
+			  BLYNK_LOG("Certificate doesn't match");
+			  return false;
+		  } else {
+			  BLYNK_LOG("Certificate OK");
+		  }
+		  return true;
+        }
+        return false;
+    }
+
+private:
+    const char* fingerprint;
+};
+
+template <typename Transport>
+class BlynkWifi
+    : public BlynkProtocol<Transport>
+{
+    typedef BlynkProtocol<Transport> Base;
+public:
+    BlynkWifi(Transport& transp)
+        : Base(transp)
+    {}
+
+    void connectWiFi(const char* ssid, const char* pass)
+    {
+        BLYNK_LOG("Connecting to %s", ssid);
+        if (pass && strlen(pass)) {
+        	WiFi.begin(ssid, pass);
+        } else {
+        	WiFi.begin(ssid);
+        }
+        while (WiFi.status() != WL_CONNECTED) {
+            ::delay(500);
+        }
+        BLYNK_LOG("Connected to WiFi");
+
+        IPAddress myip = WiFi.localIP();
+        BLYNK_LOG("My IP: %d.%d.%d.%d", myip[0], myip[1], myip[2], myip[3]);
+    }
+
+    void config(const char* auth,
+                const char* domain = BLYNK_DEFAULT_DOMAIN,
+                uint16_t    port   = BLYNK_DEFAULT_PORT_SSL,
+				const char* fingerprint = BLYNK_DEFAULT_FINGERPRINT)
+    {
+        Base::begin(auth);
+        this->conn.begin(domain, port);
+        this->conn.setFingerprint(fingerprint);
+    }
+
+    void config(const char* auth,
+            	IPAddress   ip,
+                uint16_t    port = BLYNK_DEFAULT_PORT_SSL,
+				const char* fingerprint = BLYNK_DEFAULT_FINGERPRINT)
+    {
+        Base::begin(auth);
+        this->conn.begin(ip, port);
+        this->conn.setFingerprint(fingerprint);
+    }
+
+    void begin(const char* auth,
+               const char* ssid,
+               const char* pass,
+               const char* domain = BLYNK_DEFAULT_DOMAIN,
+               uint16_t    port   = BLYNK_DEFAULT_PORT_SSL,
+			   const char* fingerprint = BLYNK_DEFAULT_FINGERPRINT)
+    {
+        connectWiFi(ssid, pass);
+        config(auth, domain, port, fingerprint);
+    }
+
+    void begin(const char* auth,
+               const char* ssid,
+               const char* pass,
+               IPAddress   ip,
+               uint16_t    port   = BLYNK_DEFAULT_PORT_SSL,
+			   const char* fingerprint = BLYNK_DEFAULT_FINGERPRINT)
+    {
+        connectWiFi(ssid, pass);
+        config(auth, ip, port, fingerprint);
+    }
+
+};
+
+static WiFiClientSecure _blynkWifiClient;
+static BlynkArduinoClientSecure<WiFiClientSecure> _blynkTransport(_blynkWifiClient);
+BlynkWifi<BlynkArduinoClientSecure<WiFiClientSecure> > Blynk(_blynkTransport);
+
+#include <BlynkWidgets.h>
+
+#endif
---
+++ examples/BoardsAndShields/ESP8266_Standalone_SSL/ESP8266_Standalone_SSL.ino
@@ -0,0 +1,46 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ * This example runs directly on ESP8266 chip.
+ *
+ * You need to install this for ESP8266 development:
+ *   https://github.com/esp8266/Arduino
+ *
+ * Please be sure to select hte right ESP8266 module
+ * in the Tools -> Board menu!
+ *
+ * Change WiFi ssid, pass, and Blynk auth token to run :)
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266_SSL.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, "ssid", "pass");
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
