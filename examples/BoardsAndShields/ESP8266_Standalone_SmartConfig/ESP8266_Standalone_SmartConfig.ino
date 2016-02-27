0d58e4b819561e04fc6f22b5f70c2461a3525ad9
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-02-27 22:49:56 +0200

    SmartConfig example

---
+++ examples/BoardsAndShields/ESP8266_Standalone_SmartConfig/ESP8266_Standalone_SmartConfig.ino
@@ -0,0 +1,73 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ * This example runs directly on ESP8266 chip.
+ *
+ * You need to install this for ESP8266 development:
+ *   https://github.com/esp8266/Arduino
+ *
+ * Please be sure to select the right ESP8266 module
+ * in the Tools -> Board menu!
+ * 
+ * NOTE: SmartConfig might not work in your environment.
+ *       Please try basic ESP8266 SmartConfig examples
+ *       before using this sketch!
+ *
+ * Change Blynk auth token to run :)
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  Serial.begin(9600);
+
+  WiFi.mode(WIFI_STA);
+
+  int cnt = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (cnt++ >= 10) {
+      WiFi.beginSmartConfig();
+      while (1) {
+        delay(1000);
+        if (WiFi.smartConfigDone()) {
+          Serial.println("");
+          BLYNK_LOG("SmartConfig: Success");
+          break;
+        }
+        Serial.print("|");
+      }
+    }
+  }
+
+  WiFi.printDiag(Serial);
+
+  Blynk.config(auth);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
