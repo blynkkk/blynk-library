4abbd832188b6189ecabd99a762c66d2a1b7e9d0
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-03-08 16:06:23 +0200

    Add MKR1000 support

---
+++ BlynkSimpleMKR1000.h
@@ -0,0 +1,24 @@
+/**
+ * @file       BlynkSimpleMKR1000.h
+ * @author     Volodymyr Shymanskyy
+ * @license    This project is released under the MIT License (MIT)
+ * @copyright  Copyright (c) 2015 Volodymyr Shymanskyy
+ * @date       Mar 2016
+ * @brief
+ *
+ */
+
+#ifndef BlynkSimpleMKR1000_h
+#define BlynkSimpleMKR1000_h
+
+#ifndef BLYNK_INFO_DEVICE
+#define BLYNK_INFO_DEVICE  "MKR1000"
+#endif
+
+#ifndef BLYNK_INFO_CONNECTION
+#define BLYNK_INFO_CONNECTION  "WiFi"
+#endif
+
+#include <BlynkSimpleWiFiShield101.h>
+
+#endif
