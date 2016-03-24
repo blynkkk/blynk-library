67bf35620bae3a5a9e8eebcbf11ee7a1af235f87
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-03-25 01:28:17 +0200

    Fix #92

--- examples/More/Sync/Sync.ino
+++ examples/More/Sync/Sync.ino
@@ -44,7 +44,12 @@ bool isFirstConnect = true;
 // This function will run every time Blynk connection is established
 BLYNK_CONNECTED() {
   if (isFirstConnect) {
+    // Request Blynk server to re-send latest values for all pins
     Blynk.syncAll();
+
+    // You can also update an individual Virtual pin like this:
+    //Blynk.syncVirtual(V0);
+
     isFirstConnect = false;
   }
 
