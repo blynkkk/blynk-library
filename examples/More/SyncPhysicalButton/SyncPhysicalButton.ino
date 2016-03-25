39cd6ba0b19d724166e320da1f0b785c8f8e5642
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-03-25 02:33:02 +0200

    Fix #82

---
+++ examples/More/SyncPhysicalButton/SyncPhysicalButton.ino
@@ -0,0 +1,100 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ * This example shows how to synchronize Button widget 
+ * and physical button state.
+ *
+ * WARNING :
+ * For this example you'll need SimpleTimer library:
+ *   https://github.com/jfturcot/SimpleTimer
+ *
+ * App project setup:
+ *   Button widget attached to V2 (Switch mode)
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <SPI.h>
+#include <Ethernet.h>
+#include <BlynkSimpleEthernet.h>
+#include <SimpleTimer.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+// Set your LED and physical button pins here
+const int ledPin = 7;
+const int btnPin = 8;
+
+SimpleTimer timer;
+void checkPhysicalButton();
+
+int ledState = LOW;
+int btnState = HIGH;
+
+void setup()
+{
+  Serial.begin(9600); // See the connection status in Serial Monitor
+  Blynk.begin(auth);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(btnPin, INPUT_PULLUP); 
+  digitalWrite(ledPin, ledState);
+
+  // Setup a function to be called every 100 ms
+  timer.setInterval(100L, checkPhysicalButton);
+}
+
+// Every time we connect to the cloud...
+BLYNK_CONNECTED() {
+  // Request the latest state from the server
+  Blynk.syncVirtual(V2);
+
+  // Alternatively, you could override server state using:
+  //Blynk.virtualWrite(V2, ledState);
+}
+
+// When App button is pushed - switch the state
+BLYNK_WRITE(V2) {
+  ledState = param.asInt();
+  digitalWrite(ledPin, ledState);
+}
+
+void checkPhysicalButton()
+{
+  if (digitalRead(btnPin) == LOW) {
+    // btnState is used to avoid sequential toggles
+    if (btnState != LOW) {
+      
+      // Toggle LED state
+      ledState = !ledState;
+      digitalWrite(ledPin, ledState);
+
+      // Update Button Widget
+      Blynk.virtualWrite(V2, ledState);
+    }
+    btnState = LOW;
+  } else {
+    btnState = HIGH;
+  }
+}
+
+void loop()
+{
+  Blynk.run(); // Run Blynk
+  timer.run(); // Run SimpleTimer
+}
+
