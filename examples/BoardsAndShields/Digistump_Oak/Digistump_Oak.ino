5db376a48e26973270ccb5baf9aeb3d673ad4f7d
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-05-01 15:11:59 +0300

    Add Oak example

---
+++ examples/BoardsAndShields/Digistump_Oak/Digistump_Oak.ino
@@ -0,0 +1,44 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ * This example shows how to use Digistump Oak with Blynk.
+ *
+ * Please be sure to select the right board type
+ * in the Tools -> Board menu!
+ *
+ * Change Blynk auth token to run :)
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  Serial.begin(9600);
+  // Oak has already configured WiFi
+  Blynk.config(auth);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
