ad118dddd7f00c7420e521f3c54e5b2d16c1a269
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-05-06 12:33:22 +0300

    Fix linux build

--- linux/BlynkApiLinux.h
+++ linux/BlynkApiLinux.h
@@ -27,6 +27,14 @@ void delay(unsigned long ms)
     usleep(ms * 1000);
 }
 
+static
+millis_time_t millis()
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts );
+    return ( ts.tv_sec * 1000 + ts.tv_nsec / 1000000L );
+}
+
 template<class Proto>
 void BlynkApi<Proto>::Init()
 {
@@ -36,9 +44,7 @@ template<class Proto>
 BLYNK_FORCE_INLINE
 millis_time_t BlynkApi<Proto>::getMillis()
 {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts );
-    return ( ts.tv_sec * 1000 + ts.tv_nsec / 1000000L );
+    return millis();
 }
 
 #ifdef BLYNK_NO_INFO
