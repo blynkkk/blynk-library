c6b0a8e5580b019624947f873df6206cf6f3ef55
Blynk <pavel@blynk.cc>
2016-06-04 18:02:39 -0400

    Update Bridge.ino

--- examples/Widgets/Bridge/Bridge.ino
+++ examples/Widgets/Bridge/Bridge.ino
@@ -69,20 +69,17 @@ void blynkAnotherDevice() // Here we will send HIGH or LOW once per second
   if (value) {
     bridge1.digitalWrite(9, HIGH);  // Digital Pin 9 on the second board will be set HIGH
     bridge1.virtualWrite(V5, 1); // Virtual Pin 5 on the second board will be set HIGH
-/////////////////////////////////////////////////////////////////////////////////////////
-// Keep in mind that when performing virtualWrite with Bridge,
-// second board will need to process the incoming command.
-// It can be done by using this handler on the second board:
-//
-//    BLYNK_WRITE(V5)
-//    {
-//    int pinData = param.asInt(); // pinData variable will store value that came via Bridge
-//    }
-//
-/////////////////////////////////////////////////////////////////////////////////////////
-}
-
-
+  
+  /////////////////////////////////////////////////////////////////////////////////////////
+  // Keep in mind that when performing virtualWrite with Bridge,
+  // second board will need to process the incoming command.
+  // It can be done by using this handler on the second board:
+  //
+  //    BLYNK_WRITE(V5){
+  //    int pinData = param.asInt(); // pinData variable will store value that came via Bridge
+  //    }
+  //
+  /////////////////////////////////////////////////////////////////////////////////////////
   } else {
     bridge1.digitalWrite(9, LOW); // Digital Pin 9 on the second board will be set LOW
     bridge1.virtualWrite(V5, 2); // Virtual Pin 5 on the second board will be set HIGH
