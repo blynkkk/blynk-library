d786c7d43006cff0fd2df18344015b0bde008f1e
Blynk <pavel@blynk.cc>
2016-06-14 07:46:56 -0400

    Update GetData.ino

--- examples/GettingStarted/GetData/GetData.ino
+++ examples/GettingStarted/GetData/GetData.ino
@@ -36,19 +36,19 @@ void setup()
   Blynk.begin(auth);
 }
 
-// This function will be called every time
-// when App writes value to Virtual Pin 1
+// This function will be called every time Slider Widget 
+// in Blynk app writes values to the Virtual Pin 1
 BLYNK_WRITE(V1)
 {
-  Serial.print("Got a value: ");
-  Serial.println(param.asStr());
+  pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
   // You can also use:
-  // int i = param.asInt() or
-  // double d = param.asDouble()
+  // String i = param.asStr();
+  // double d = param.asDouble();
+  Serial.print("V1 Slider value is: ");
+  Serial.println(pinValue);
 }
 
 void loop()
 {
   Blynk.run();
 }
-
