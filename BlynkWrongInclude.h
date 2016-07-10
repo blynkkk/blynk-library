f5158cb90de10c90510efb7b5e75b9e9da3058e7
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-07-10 14:54:01 +0300

    Add WrongInclude

---
+++ BlynkWrongInclude.h
@@ -0,0 +1,16 @@
+/**
+ * @file       BlynkWrongInclude.h
+ * @author     Volodymyr Shymanskyy
+ * @license    This project is released under the MIT License (MIT)
+ * @copyright  Copyright (c) 2015 Volodymyr Shymanskyy
+ * @date       Jun 2016
+ * @brief
+ *
+ */
+
+#ifndef BlynkWrongInclude_h
+#define BlynkWrongInclude_h
+
+#error Please include a board-specific header file (see examples)
+
+#endif
