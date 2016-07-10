1ed192d83c57e8cf02439fc0b414df80a441a976
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-07-10 14:56:01 +0300

    Add support for Fishino boards

---
+++ BlynkSimpleFishino.h
@@ -0,0 +1,113 @@
+/**
+ * @file       BlynkSimpleFishino.h
+ * @author     Volodymyr Shymanskyy
+ * @license    This project is released under the MIT License (MIT)
+ * @copyright  Copyright (c) 2015 Volodymyr Shymanskyy
+ * @date       Jun 2016
+ * @brief
+ *
+ */
+
+#ifndef BlynkSimpleFishino_h
+#define BlynkSimpleFishino_h
+
+#ifndef BLYNK_INFO_CONNECTION
+#define BLYNK_INFO_CONNECTION "Fishino"
+#endif
+
+#ifdef ESP8266
+#error This code is not intended to run on the ESP8266 platform! Please check your Tools->Board setting.
+#endif
+
+#include <BlynkApiArduino.h>
+#include <Blynk/BlynkProtocol.h>
+#include <Adapters/BlynkArduinoClient.h>
+#include <Fishino.h>
+
+class BlynkFishino
+    : public BlynkProtocol<BlynkArduinoClient>
+{
+    typedef BlynkProtocol<BlynkArduinoClient> Base;
+public:
+    BlynkFishino(BlynkArduinoClient& transp)
+        : Base(transp)
+    {}
+
+    void connectWiFi(const char* ssid, const char* pass)
+    {
+        BLYNK_LOG2(BLYNK_F("Connecting to "), ssid);
+    	SPI.begin();
+    	SPI.setClockDivider(SPI_CLOCK_DIV2);
+
+    	while(!Fishino.reset()) {
+    		BLYNK_LOG1(BLYNK_F("Fishino RESET FAILED"));
+    	}
+
+    	Fishino.setMode(STATION_MODE);
+        if (pass && strlen(pass)) {
+            while (!Fishino.begin(ssid, pass)) {
+                ::delay(500);
+            }
+        } else {
+            while (!Fishino.begin(ssid)) {
+                ::delay(500);
+            }
+        }
+
+        BLYNK_LOG1(BLYNK_F("Connected to WiFi"));
+
+        Fishino.staStartDHCP();
+    	while(Fishino.status() != STATION_GOT_IP)
+    	{
+    		::delay(500);
+    	}
+
+        IPAddress myip = Fishino.localIP();
+        BLYNK_LOG_IP("IP: ", myip);
+    }
+
+    void config(const char* auth,
+                const char* domain = BLYNK_DEFAULT_DOMAIN,
+                uint16_t    port   = BLYNK_DEFAULT_PORT)
+    {
+        Base::begin(auth);
+        this->conn.begin(domain, port);
+    }
+
+    void config(const char* auth,
+            	IPAddress   ip,
+                uint16_t    port = BLYNK_DEFAULT_PORT)
+    {
+        Base::begin(auth);
+        this->conn.begin(ip, port);
+    }
+
+    void begin(const char* auth,
+               const char* ssid,
+               const char* pass,
+               const char* domain = BLYNK_DEFAULT_DOMAIN,
+               uint16_t    port   = BLYNK_DEFAULT_PORT)
+    {
+        connectWiFi(ssid, pass);
+        config(auth, domain, port);
+    }
+
+    void begin(const char* auth,
+               const char* ssid,
+               const char* pass,
+               IPAddress   ip,
+               uint16_t    port   = BLYNK_DEFAULT_PORT)
+    {
+        connectWiFi(ssid, pass);
+        config(auth, ip, port);
+    }
+
+};
+
+static FishinoClient _blynkFishinoClient;
+static BlynkArduinoClient _blynkTransport(_blynkFishinoClient);
+BlynkFishino Blynk(_blynkTransport);
+
+#include <BlynkWidgets.h>
+
+#endif
---
+++ examples/Boards_WiFi/Fishino/Fishino.ino
@@ -0,0 +1,50 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ *
+ * This example shows how to use Fishino to connect your project to Blynk.
+ *
+ * You need to install Fishino Software libraries:
+ *   http://fishino.it/en/download/
+ *
+ * Change WiFi ssid, pass, and Blynk auth token to run :)
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <SPI.h>
+#include <Fishino.h>
+#include <BlynkSimpleFishino.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+// Your WiFi configuration.
+// Set password to "" for open networks
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
