70485af21cf8e1ec76fc2e5aef50b76a35d3fc40
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-08-30 09:18:34 +0300

    format

--- examples/Boards_BLE/RedBear_Duo_BLE/RedBear_Duo_BLE.ino
+++ examples/Boards_BLE/RedBear_Duo_BLE/RedBear_Duo_BLE.ino
@@ -22,9 +22,9 @@
  **************************************************************/
 
 #if defined(ARDUINO)
-  SYSTEM_MODE(MANUAL);    // If Arduino - do not connect to Particle cloud
+SYSTEM_MODE(MANUAL);    // If Arduino - do not connect to Particle cloud
 #else
-  SYSTEM_MODE(AUTOMATIC); // Otherwise, connect to Particle cloud
+SYSTEM_MODE(AUTOMATIC); // Otherwise, connect to Particle cloud
 #endif
 
 //#define BLYNK_DEBUG
