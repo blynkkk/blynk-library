f69ffade852a97c095e1634706865caff13fa9ff
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-09-20 18:58:09 +0300

    Add STM32F103 Blue Pill example

---
+++ examples/Boards_USB_Serial/Blue_Pill_STM32F103C8/Blue_Pill_STM32F103C8.ino
@@ -0,0 +1,47 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ * This example shows how to use ordinary Arduino Serial
+ * to connect your project to Blynk.
+ * Feel free to apply it to any other example. It's simple!
+ *
+ * Requires STM32duino: https://github.com/rogerclarkmelbourne/Arduino_STM32/wiki/Installation
+ *
+ **************************************************************
+ *            USB HOWTO: http://tiny.cc/BlynkUSB
+ **************************************************************/
+
+#define BLYNK_PRINT Serial2
+#include <BlynkSimpleStream.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  // Debug console
+  Serial2.begin(9600);
+
+  // Blynk will work through Serial
+  Serial.begin(9600);
+  Blynk.begin(auth, Serial);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
