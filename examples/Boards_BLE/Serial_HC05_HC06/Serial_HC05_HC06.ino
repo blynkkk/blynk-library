b34f772518509faa3ad4c518815ec765fcd50289
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-09-24 23:39:05 +0300

    Add Bluetooth 2.0 Serial Port Profile (SPP) example

---
+++ examples/Boards_BLE/Serial_HC05_HC06/Serial_HC05_HC06.ino
@@ -0,0 +1,51 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ * This example shows how to use Arduino with HC-06/HC-05
+ * Bluetooth 2.0 Serial Port Profile (SPP) module
+ * to connect your project to Blynk.
+ *
+ * Feel free to apply it to any other example. It's simple!
+ *
+ * NOTE: Bluetooth support is in beta!
+ *
+ **************************************************************/
+
+// You could use a spare Hardware Serial on boards that have it (like Mega)
+#include <SoftwareSerial.h>
+SoftwareSerial DebugSerial(2, 3); // RX, TX
+
+#define BLYNK_PRINT DebugSerial
+#include <BlynkSimpleStream.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  // Debug console
+  DebugSerial.begin(9600);
+
+  // Blynk will work through Serial
+  Serial.begin(9600);
+  Blynk.begin(auth, Serial);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
