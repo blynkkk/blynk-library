96d89e8708011d08b52ef476a50286898eab681e
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-10-03 12:27:57 +0300

    Update comments

--- examples/Export_Demo/Template_ESP8266/Template_ESP8266.ino
+++ examples/Export_Demo/Template_ESP8266/Template_ESP8266.ino
@@ -3,14 +3,14 @@
  * You should open Setting.h and modify General options.
  *
  * If you would like to add these features to your product,
- * please contact Blynk for Business:
+ * please contact Blynk for Businesses:
  *
  *                  http://www.blynk.io/
  *
  **************************************************************/
 
-#define USE_SPARKFUN_BLYNK_BOARD
-//#define USE_NODE_MCU_BOARD
+#define USE_SPARKFUN_BLYNK_BOARD  // Uncomment the board you are using 
+//#define USE_NODE_MCU_BOARD      // Comment out the boards you are not using
 //#define USE_WITTY_CLOUD_BOARD
 
 #define DEBUG        // Comment this out to disable debug prints
