160ed50e3700de785aecf16b33b2c07cda14436c
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-10-22 15:07:03 +0300

    Fix tabs [ci skip]

--- src/BlynkApiArduino.h
+++ src/BlynkApiArduino.h
@@ -26,10 +26,10 @@ millis_time_t BlynkApi<Proto>::getMillis()
 // TODO: Remove workaround for Intel Curie
 // https://forum.arduino.cc/index.php?topic=391836.0
 #ifdef ARDUINO_ARCH_ARC32
-	noInterrupts();
-	uint64_t t = millis();
-	interrupts();
-	return t;
+    noInterrupts();
+    uint64_t t = millis();
+    interrupts();
+    return t;
 #else
     return millis();
 #endif
