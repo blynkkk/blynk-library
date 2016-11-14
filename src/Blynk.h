6439f141a0bd341496e1479d17c6b10237a20675
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-11-14 13:27:03 +0200

    Add Blynk.h, Fix #121

---
+++ src/Blynk.h
@@ -0,0 +1,17 @@
+/**
+ * @file       Blynk.h
+ * @author     Volodymyr Shymanskyy
+ * @license    This project is released under the MIT License (MIT)
+ * @copyright  Copyright (c) 2015 Volodymyr Shymanskyy
+ * @date       Nov 2016
+ * @brief
+ *
+ */
+
+#ifndef Blynk_h
+#define Blynk_h
+
+#include <Blynk/BlynkApi.h>
+#warning "Please include a board-specific header file, instead of Blynk.h (see examples)"
+
+#endif
