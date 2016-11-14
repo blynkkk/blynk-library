497784eab1d0c3d50005188f4b68ea94f4d6ce6e
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-11-14 13:43:35 +0200

    Update JSON generator

--- tests/GenerateJSON/GenerateJSON.ino
+++ tests/GenerateJSON/GenerateJSON.ino
@@ -1,24 +1,24 @@
-#include "BlynkApiArduino.h"
-#include "Blynk/BlynkDetectDevice.h"
+#include <Blynk.h>
+#include <Blynk/BlynkDetectDevice.h>
 
-#if !defined(BOARD_PWM_MAX)
-  #if   defined(PWMRANGE)
-    #define BOARD_PWM_MAX    PWMRANGE
-  #elif defined(PWM_RESOLUTION)
-    #define BOARD_PWM_MAX    (2^(PWM_RESOLUTION)-1)
-  #else
-    #error "Cannot detect BOARD_PWM_MAX"
-  #endif
+/*
+ * Pins Quantity
+ */
+
+#if defined(NUM_DIGITAL_PINS)
+  #define BOARD_DIGITAL_MAX int(NUM_DIGITAL_PINS)
+#elif defined(PINS_COUNT)
+  #define BOARD_DIGITAL_MAX int(PINS_COUNT)
+#else
+  #warning "BOARD_DIGITAL_MAX not detected"
+  #define BOARD_DIGITAL_MAX 32
 #endif
 
-#if !defined(BOARD_ANALOG_MAX)
-  #if   defined(PWMRANGE)
-    #define BOARD_ANALOG_MAX    PWMRANGE
-  #elif defined(PWM_RESOLUTION)
-    #define BOARD_ANALOG_MAX    (2^(PWM_RESOLUTION)-1)
-  #else
-    #error "Cannot detect BOARD_ANALOG_MAX"
-  #endif
+#if defined(NUM_ANALOG_INPUTS)
+  #define BOARD_ANALOG_IN_MAX int(NUM_ANALOG_INPUTS)
+#else
+  #warning "BOARD_ANALOG_IN_MAX not detected"
+  #define BOARD_ANALOG_IN_MAX 0
 #endif
 
 #if defined(BLYNK_USE_128_VPINS)
@@ -27,96 +27,157 @@
   #define BOARD_VIRTUAL_MAX 31
 #endif
 
+/*
+ * Pins Functions
+ */
+
+#ifndef digitalPinHasPWM
+  #warning "No digitalPinHasPWM"
+  #define digitalPinHasPWM(x) false
+#endif
+
+#if !defined(analogInputToDigitalPin)
+  #warning "No analogInputToDigitalPin"
+  #define analogInputToDigitalPin(x) -1
+#endif
+
+/*
+ * Pins Ranges
+ */
+
+#if !defined(BOARD_PWM_MAX)
+  #if   defined(PWMRANGE)
+    #define BOARD_PWM_MAX    PWMRANGE
+  #elif defined(PWM_RESOLUTION)
+    #define BOARD_PWM_MAX    ((2^(PWM_RESOLUTION))-1)
+  #else
+    #warning "Cannot detect BOARD_PWM_MAX"
+    #define BOARD_PWM_MAX    255
+  #endif
+#endif
+
+#if !defined(BOARD_ANALOG_MAX)
+  #if   defined(ADC_RESOLUTION)
+    #define BOARD_ANALOG_MAX    ((2^(ADC_RESOLUTION))-1)
+  #else
+    #warning "Cannot detect BOARD_ANALOG_MAX"
+    #define BOARD_ANALOG_MAX    1023
+  #endif
+#endif
+
+#if defined(clockCyclesPerMicrosecond)
+  #define BOARD_INFO_MHZ clockCyclesPerMicrosecond()
+#elif defined(F_CPU)
+  #define BOARD_INFO_MHZ ((F_CPU)/1000000UL)
+#endif
+
+
+struct Ser {
+  template<typename T, typename... Args>
+  void print(T last) {
+    Serial.print(last);
+  }
+  
+  template<typename T, typename... Args>
+  void print(T head, Args... tail) {
+    Serial.print(head);
+    print(tail...);
+  }
+} ser;
+
 const char* JS[] = {
-R"raw(
-{
-    "name": ")raw", R"raw(",
-    "map": {
-        "digital": {
-            "pins": {
-                 )raw", R"raw(
-            },
-            "ops": [ "dr", "dw" ]
-        },
-        "analog": {
-            "pins": {
-                 )raw", R"raw(
-            },
-            "ops": [ "dr", "dw", "ar" ],
-            "arRange": [ 0, )raw", R"raw( ]
-        },
-        "pwm": {
-            "pins": [
-                )raw", R"raw(
-            ],
-            "ops": [ "aw" ],
-            "awRange": [ 0, )raw", R"raw( ]
-        },
-        "virtual":  {
-            "pinsRange": [ 0, )raw", R"raw( ],
-            "ops": [ "vr", "vw" ]
-        }
-    }
-}
-)raw" };
-            
+"\n"
+"{\n"
+"    ",
+     "\"map\": {\n"
+"        \"digital\": {\n"
+"            \"pins\": {\n"
+"                 ", "\n"
+"            },\n"
+"            \"ops\": [ \"dr\", \"dw\" ]\n"
+"        },\n"
+"        \"analog\": {\n"
+"            \"pins\": {\n"
+"                 ", "\n"
+"            },\n"
+"            \"ops\": [ \"dr\", \"dw\", \"ar\" ],\n"
+"            \"arRange\": [ 0, ", " ]\n"
+"        },\n"
+"        \"pwm\": {\n"
+"            \"pins\": [\n"
+"                ", "\n"
+"            ],\n"
+"            \"ops\": [ \"aw\" ],\n"
+"            \"awRange\": [ 0, ", " ]\n"
+"        },\n"
+"        \"virtual\":  {\n"
+"            \"pinsRange\": [ 0, ", " ],\n"
+"            \"ops\": [ \"vr\", \"vw\" ]\n"
+"        }\n"
+"    }\n"
+"}\n"
+};
+
+
+
 void setup() {
   Serial.begin(9600);
   delay(10);
 }
 
 void loop() {
-  Serial.print(JS[0]);
-  Serial.print(BLYNK_INFO_DEVICE);
-  Serial.print(JS[1]);
+  ser.print(JS[0]);
+  ser.print("\"name\": \"", BLYNK_INFO_DEVICE, "\",\n    ");
+#ifdef BLYNK_INFO_CPU
+  ser.print("\"cpu\": \"",  BLYNK_INFO_CPU, "\",\n    ");
+#endif
+#ifdef BOARD_INFO_MHZ
+  ser.print("\"mhz\": ",  BOARD_INFO_MHZ, ",\n    ");
+#endif
+  ser.print(JS[1]);
 
-  for (int i = 0; i < NUM_DIGITAL_PINS; i++) {
-    Serial.print("\"D");
-    Serial.print(i);
-    Serial.print("\": ");
-    Serial.print(i);
+  for (int i = 0; i < BOARD_DIGITAL_MAX; i++) {
+    ser.print("\"D", i, "\": ", i);
     if (i % 5 != 4) {
-      Serial.print(", ");
+      ser.print(", ");
     } else {
-      Serial.print(",\n                 ");
+      ser.print(",\n                 ");
     }
   }
 
-  Serial.print(JS[2]);
+  ser.print(JS[2]);
 
-  for (int i = 0; i < NUM_ANALOG_INPUTS; i++) {
+  for (int i = 0; i < BOARD_ANALOG_IN_MAX; i++) {
     int pin = analogInputToDigitalPin(i);
-    Serial.print("\"A");
-    Serial.print(i);
-    Serial.print("\": ");
-    Serial.print(pin);
-    if (i % 5 != 4) {
-      Serial.print(", ");
-    } else {
-      Serial.print(",\n                 ");
+    if (pin != -1) {
+      ser.print("\"A", i, "\": ", pin);
+      if (i % 5 != 4) {
+        ser.print(", ");
+      } else {
+        ser.print(",\n                 ");
+      }
     }
   }
 
-  Serial.print(JS[3]);
-  Serial.print(BOARD_ANALOG_MAX);
-  Serial.print(JS[4]);
+  ser.print(JS[3]);
+  ser.print(BOARD_ANALOG_MAX);
+  ser.print(JS[4]);
 
-  for (int i = 0; i < NUM_DIGITAL_PINS; i++) {
+  for (int i = 0; i < BOARD_DIGITAL_MAX; i++) {
     bool hasPWM = digitalPinHasPWM(i);
-    bool hasInt = digitalPinToInterrupt(i) != NOT_AN_INTERRUPT;
+    //bool hasInt = digitalPinToInterrupt(i) != NOT_AN_INTERRUPT;
 
     if (hasPWM) {
-      Serial.print("\"D");
-      Serial.print(i);
-      Serial.print("\", ");
+      ser.print("\"D", i, "\", ");
     }
   }
 
-  Serial.print(JS[5]);
-  Serial.print(BOARD_PWM_MAX);
-  Serial.print(JS[6]);
-  Serial.print(BOARD_VIRTUAL_MAX);
-  Serial.print(JS[7]);
+  ser.print(JS[5]);
+  ser.print(BOARD_PWM_MAX);
+  ser.print(JS[6]);
+  ser.print(BOARD_VIRTUAL_MAX);
+  ser.print(JS[7]);
+
   delay(10000);
 }
 
