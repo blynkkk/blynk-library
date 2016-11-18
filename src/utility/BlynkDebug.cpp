cee8c002b631ed31124fafef85f3035eb8322c13
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-11-18 16:42:30 +0200

    Update debug

--- src/utility/BlynkDebug.cpp
+++ src/utility/BlynkDebug.cpp
@@ -6,27 +6,85 @@
  * @date       Jan 2015
  * @brief      Debug utilities for Arduino
  */
-
 #include <Blynk/BlynkDebug.h>
-#include <Arduino.h>
 
-size_t BlynkFreeRam()
-{
 #if defined(__AVR__)
-    extern int __heap_start, *__brkval;
-    int v;
-    return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
-#else
-    return 0;
-#endif
-}
 
-void BlynkReset()
-{
-    void(*resetFunc)(void) = 0;
-    resetFunc();
-    for(;;); // To make compiler happy
-}
+    #include <Arduino.h>
+
+	size_t BlynkFreeRam()
+	{
+		extern int __heap_start, *__brkval;
+		int v;
+		return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+	}
+
+	void BlynkReset()
+	{
+		void(*resetFunc)(void) = 0;
+		resetFunc();
+		for(;;) {} // To make compiler happy
+	}
+
+#elif defined(ESP8266)
+
+    #include <Arduino.h>
+
+	size_t BlynkFreeRam()
+	{
+		return ESP.getFreeHeap();
+	}
+
+	void BlynkReset()
+	{
+		ESP.restart();
+		for(;;) {} // To make compiler happy
+	}
+
+#elif defined(__DUE__) //TODO
+
+    #include <Arduino.h>
+
+	size_t BlynkFreeRam()
+	{
+		return 0;
+	}
+	void BlynkReset()
+	{
+		NVIC_SystemReset();
+		for(;;) {} // To make compiler happy
+	}
+
+#elif defined (PARTICLE) || defined(SPARK)
+
+    #include "application.h"
+
+    size_t BlynkFreeRam()
+    {
+        return 0;
+    }
+
+    void BlynkReset()
+    {
+        System.reset();
+        for(;;) {} // To make compiler happy
+    }
+
+#else
+
+	#error "Need to implement board-specific utilities"
+
+	size_t BlynkFreeRam()
+	{
+		return 0;
+	}
+
+	void BlynkReset()
+	{
+		for(;;) {} // To make compiler happy
+	}
+
+#endif
 
 void BlynkFatal()
 {
