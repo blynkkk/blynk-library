c0d112789fbfa270450ea473fc45479afac87ab1
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-12-01 16:11:27 +0200

    add setClient()

--- src/Adapters/BlynkArduinoClient.h
+++ src/Adapters/BlynkArduinoClient.h
@@ -25,10 +25,19 @@ template <typename Client>
 class BlynkArduinoClientGen
 {
 public:
-    BlynkArduinoClientGen(Client& client)
-        : client(client), domain(NULL), port(0), isConn(false)
+    BlynkArduinoClientGen(Client& c)
+        : client(NULL), domain(NULL), port(0), isConn(false)
     {
-        client.setTimeout(BLYNK_TIMEOUT_MS);
+        setClient(&c);
+    }
+
+    BlynkArduinoClientGen()
+        : client(NULL), domain(NULL), port(0), isConn(false)
+    {}
+
+    void setClient(Client* c) {
+        client = c;
+        client->setTimeout(BLYNK_TIMEOUT_MS);
     }
 
     void begin(IPAddress a, uint16_t p) {
@@ -46,26 +55,26 @@ public:
         if (domain) {
             BLYNK_LOG4(BLYNK_F("Connecting to "), domain, ':', port);
 
-            isConn = (1 == client.connect(domain, port));
+            isConn = (1 == client->connect(domain, port));
             return isConn;
         } else { //if (uint32_t(addr) != 0) {
             BLYNK_LOG_IP("Connecting to ", addr);
-            isConn = (1 == client.connect(addr, port));
+            isConn = (1 == client->connect(addr, port));
             return isConn;
         }
         return false;
     }
 
-    void disconnect() { isConn = false; client.stop(); }
+    void disconnect() { isConn = false; client->stop(); }
 
 #ifdef BLYNK_ENC28J60_FIX
     size_t read(void* buf, size_t len) {
-        while (client.available() < len) { }
-        return client.read((uint8_t*)buf, len);
+        while (client->available() < len) { }
+        return client->read((uint8_t*)buf, len);
     }
 #else
     size_t read(void* buf, size_t len) {
-        size_t res = client.readBytes((char*)buf, len);
+        size_t res = client->readBytes((char*)buf, len);
         YIELD_FIX();
         return res;
     }
@@ -76,7 +85,7 @@ public:
         size_t sent = 0;
         int retry = 0;
         while (sent < len && ++retry < 10) {
-            size_t w = client.write((const uint8_t*)buf+sent, len-sent);
+            size_t w = client->write((const uint8_t*)buf+sent, len-sent);
             if (w != 0 && w != -1) {
                 sent += w;
             } else {
@@ -97,17 +106,17 @@ public:
 #else
     size_t write(const void* buf, size_t len) {
         YIELD_FIX();
-        size_t res = client.write((const uint8_t*)buf, len);
+        size_t res = client->write((const uint8_t*)buf, len);
         YIELD_FIX();
         return res;
     }
 #endif
 
-    bool connected() { YIELD_FIX(); return isConn && client.connected(); }
-    int available() {  YIELD_FIX(); return client.available(); }
+    bool connected() { YIELD_FIX(); return isConn && client->connected(); }
+    int available() {  YIELD_FIX(); return client->available(); }
 
 protected:
-    Client&     client;
+    Client*     client;
     IPAddress   addr;
     const char* domain;
     uint16_t    port;
