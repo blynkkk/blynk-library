ee39927d4fa7b59d11944ca85f45f180dd03054e
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-12-27 11:34:05 +0200

    Fix for Windows

--- extras/ide-tools/BlynkUsbScript.java
+++ extras/ide-tools/BlynkUsbScript.java
@@ -33,6 +33,8 @@ import processing.app.Base;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.text.DefaultCaret;
+import java.nio.file.*;
 
 import processing.app.Theme;
 
@@ -50,6 +52,15 @@ public class BlynkUsbScript implements Tool {
     return "Blynk: Run USB script";
   }
 
+  private void captureIO(final InputStream src, JTextArea textArea) {
+    new Thread(() -> {
+      Scanner sc = new Scanner(src);
+      while (sc.hasNextLine()) {
+        textArea.append(sc.nextLine() + "\n");
+      }
+      //} catch (IOException e) {
+    }).start();
+  }
 
   public void run() {
     Runnable runnable = () -> {
@@ -59,67 +70,83 @@ public class BlynkUsbScript implements Tool {
         Font editorFont = PreferencesData.getFont("editor.font");
         Font font = Theme.scale(new Font(consoleFont.getName(), consoleFont.getStyle(), editorFont.getSize()));
 
-        JTextArea textArea = new JTextArea(
-          "Ensure that Serial Monitor and Plotter are closed when using this tool.\n" +
-          "It uses same port and speed as Serial Monitor\n\n"
-        );
-        textArea.setRows(16);
-        textArea.setColumns(120);
-        textArea.setEditable(false);
-        textArea.setLineWrap(true);
-        textArea.setFont(font);
-
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        //scrollPane.setPreferredSize( new Dimension(textArea.getPreferredSize().width, 500 ) );
+        String warning = "Ensure that Serial Monitor and Plotter are closed when using this tool.\n" +
+          "It uses same port and speed as Serial Monitor\n\n";
 
         String serial_port = PreferencesData.get("serial.port");
-        String serial_serial_rate = PreferencesData.get("serial.debug_rate");
+        String serial_debug_rate = PreferencesData.get("serial.debug_rate");
         String sketchbook_path = PreferencesData.get("sketchbook.path");
 
-        String blynk_path = sketchbook_path + "/libraries/Blynk/";
-        String scripts_path = blynk_path + "/scripts/";
+        Path blynk_path = Paths.get(sketchbook_path, "libraries", "Blynk");
 
-        String script_name = "blynk-ser.sh";
+        String full_cmd;
         if (OSUtils.isWindows()) {
-          script_name = "blynk-ser.bat";
+          String script = "blynk-ser.bat";
+          Path script_path = Paths.get(blynk_path.toString(), "scripts", script);
+
+          String args = " -c " + serial_port + " -b " + serial_debug_rate;
+          full_cmd = "echo." + warning.replace("\n"," & echo.") + " & \"" + script_path.toString() + "\"" + args;
+          full_cmd = "cmd /C start \"Blynk\" cmd /C \"" + full_cmd + "\"";
+
+          //System.err.println(full_cmd);
+
+          final Process p = Runtime.getRuntime().exec(full_cmd);
+
+          p.waitFor();
+          p.destroy();
+        } else {
+
+          String script = "blynk-ser.sh";
+          Path script_path = Paths.get(blynk_path.toString(), "scripts", script);
+
+          String args = " -c " + serial_port + " -b " + serial_debug_rate;
+          full_cmd = script_path.toString() + args;
+
+          //System.err.println(full_cmd);
+
+          JTextArea textArea = new JTextArea(warning);
+          textArea.setRows(16);
+          textArea.setColumns(120);
+          textArea.setEditable(false);
+          textArea.setLineWrap(true);
+          textArea.setFont(font);
+
+          DefaultCaret caret = (DefaultCaret)textArea.getCaret();
+          caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+
+          textArea.setCaretPosition(textArea.getDocument().getLength());
+
+          JScrollPane scrollPane = new JScrollPane(textArea);
+          //scrollPane.setPreferredSize( new Dimension(textArea.getPreferredSize().width, 500 ) );
+
+          JFrame frame = new JFrame(script + args);
+          frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+          frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+          frame.pack();
+          frame.setVisible(true);
+
+          final Process p = Runtime.getRuntime().exec(full_cmd);
+
+          captureIO(p.getInputStream(), textArea);
+          captureIO(p.getErrorStream(), textArea);
+
+          frame.addWindowListener(new WindowAdapter() {
+              @Override
+              public void windowClosing(WindowEvent windowEvent) {
+                p.destroy();
+              }
+          });
+
+          p.waitFor();
+          p.destroy();
+          frame.setVisible(false);
+          frame.dispose();
         }
-        String cmd = script_name + " -c " + serial_port + " -b " + serial_serial_rate;
-
-        JFrame frame = new JFrame(cmd);
-        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-        frame.pack();
-        frame.setVisible(true);
-
-        final Process p = Runtime.getRuntime().exec(scripts_path + cmd);
-
-        new Thread(() -> {
-          BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-          try {
-            String line = null; 
-            while ((line = input.readLine()) != null) {
-              textArea.append(line + "\n");
-            }
-          } catch (IOException e) {
-          }
-        }).start();
-
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent windowEvent) {
-              p.destroy();
-            }
-        });
-
-        p.waitFor();
-        p.destroy();
-        frame.setVisible(false);
-        frame.dispose();
 
       } catch (Exception e) {
         editor.statusError("Blynk USB script failed");
         System.err.println(e);
+        //e.printStackTrace(System.err);
         return;
       }
     };
