44401552d659113ccaa0b902b6ec3e2f4949bfd9
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2017-02-01 00:09:15 +0200

    Update gsm example

--- examples/Boards_GSM/SIM800_SIM900/SIM800_SIM900.ino
+++ examples/Boards_GSM/SIM800_SIM900/SIM800_SIM900.ino
@@ -33,18 +33,23 @@
 // If you want override this value, uncomment and set this option:
 //#define BLYNK_HEARTBEAT 30
 
+// Select your modem:
+#define TINY_GSM_MODEM_SIM800
+//#define TINY_GSM_MODEM_SIM900
+//#define TINY_GSM_MODEM_M590
+
 #include <TinyGsmClient.h>
 #include <BlynkSimpleSIM800.h>
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "YourAuthToken";
+const char auth[] = "YourAuthToken";
 
 // Your GPRS credentials
 // Leave empty, if missing user or pass
-char apn[]  = "YourAPN";
-char user[] = "";
-char pass[] = "";
+const char apn[]  = "YourAPN";
+const char user[] = "";
+const char pass[] = "";
 
 // Hardware Serial on Mega, Leonardo, Micro
 #define SerialAT Serial1
@@ -67,6 +72,7 @@ void setup()
 
   // Restart takes quite some time
   // To skip it, call init() instead of restart()
+  Serial.println("Initializing modem...");
   modem.restart();
 
   // Unlock your SIM card with a PIN
