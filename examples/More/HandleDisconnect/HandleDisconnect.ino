1cd64ea216888720fd28febc05a4d72b6745a256
apokrovskyy <pokrowsckij.a@gmail.com>
2017-02-13 22:17:11 +0200

    Add HandleDisconnect example

---
+++ examples/More/HandleDisconnect/HandleDisconnect.ino
@@ -0,0 +1,60 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ * This example shows how to separate online and offline parts of your code.
+ * 
+ * You should specify your connection type and board.
+ * 
+ * This sketch uses ESP8266 libraries as example.
+ **************************************************************/
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "b737816520294f44a2c93be79eb69759";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "$oundpre$$";
+char pass[] = "andypass";
+
+
+int lastTry = millis();
+int connDelay = 5000; // Try to connect every 5 seconds
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  if(!Blynk.connected() && millis() - lastTry >= connDelay){
+    Blynk.connect();
+    lastTry = millis();
+  }
+  
+  if(Blynk.connected()){
+    Blynk.run();
+    // insert your online part
+  }
+  else{
+    //insert your offline part
+  }
+}
