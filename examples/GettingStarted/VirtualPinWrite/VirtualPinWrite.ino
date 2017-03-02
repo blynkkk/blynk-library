67d3016788723d1f91ad2b0c005173ce73f0cfe8
apokrovskyy <pokrowsckij.a@gmail.com>
2017-03-02 23:47:27 +0200

    Update examples

--- examples/GettingStarted/VirtualPinWrite/VirtualPinWrite.ino
+++ examples/GettingStarted/VirtualPinWrite/VirtualPinWrite.ino
@@ -5,10 +5,10 @@
   projects by simply dragging and dropping widgets.
 
     Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
     Blynk community:            http://community.blynk.cc
     Social networks:            http://www.fb.com/blynkapp
                                 http://twitter.com/blynk_app
-    Example generator:          http://examples.blynk.cc
 
   Blynk library is licensed under MIT license
   This example code is in public domain.
