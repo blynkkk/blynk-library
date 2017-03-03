9992eb49f3d0a1a9d62060cd12e24f984e8b8edb
Dmitriy Dumanskiy <doom369@gmail.com>
2017-03-03 14:23:05 +0200

    #279 nodeMCU sketch added.

---
+++ examples/Boards_WiFi/NodeMCU/NodeMCU.ino
@@ -0,0 +1,57 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Sketch generator:           http://examples.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ * This example runs directly on NodeMCU.
+ *
+ * Note: This requires ESP8266 support package:
+ *   https://github.com/esp8266/Arduino
+ *
+ * Please be sure to select the right NodeMCU module
+ * in the Tools -> Board menu!
+ *
+ * For advanced settings please follow ESP examples :
+ *  - ESP8266_Standalone_Manual_IP.ino
+ *  - ESP8266_Standalone_SmartConfig.ino
+ *  - ESP8266_Standalone_SSL.ino
+ *
+ * Change WiFi ssid, pass, and Blynk auth token to run :)
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
