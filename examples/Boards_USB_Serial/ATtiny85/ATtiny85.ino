4c79afc9c4bbd53f10a0bf576a572adb77d985d6
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2017-09-02 09:07:03 +0300

    Follow us

--- examples/Boards_USB_Serial/ATtiny85/ATtiny85.ino
+++ examples/Boards_USB_Serial/ATtiny85/ATtiny85.ino
@@ -10,7 +10,7 @@
     Downloads, docs, tutorials: http://www.blynk.cc
     Sketch generator:           http://examples.blynk.cc
     Blynk community:            http://community.blynk.cc
-    Social groups:              http://www.fb.com/blynkapp
+    Follow us:                  http://www.fb.com/blynkapp
                                 http://twitter.com/blynk_app
 
   Blynk library is licensed under MIT license
