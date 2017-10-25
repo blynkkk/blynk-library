a958dbbbc48939eac60b0716a21d9735d1e2a290
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2017-10-25 13:42:34 +0300

    Add WizFi310 support. Fix #352

--- src/BlynkSimpleWizFi310.h
+++ src/BlynkSimpleWizFi310.h
@@ -1,23 +1,23 @@
 /**
- * @file BlynkSimpleWifiLink.h
+ * @file BlynkSimpleWizFi310.h
  * @
  * @license This project is released under the MIT License (MIT)
  * @copyright Copyright (c) 2015 Volodymyr Shymanskyy
- * @date Jan 2017
+ * @date Oct 2017
  * @brief
  *
  */
-#ifndef BlynkSimpleWiFiLink_h
-#define BlynkSimpleWiFiLink_h
+#ifndef BlynkSimpleWizFi310_h
+#define BlynkSimpleWizFi310_h
 
 #ifndef BLYNK_INFO_CONNECTION
-#define BLYNK_INFO_CONNECTION  "WiFiLink"
+#define BLYNK_INFO_CONNECTION  "WizFi310"
 #endif
 
 #define BLYNK_SEND_ATOMIC
 #define BLYNK_SEND_CHUNK 64
 
-#include <WiFiLink.h>
+#include <WizFi310.h>
 #include <Adapters/BlynkWiFiCommon.h>
 
 static WiFiClient _blynkWifiClient;
