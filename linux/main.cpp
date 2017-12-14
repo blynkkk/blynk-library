a77e4919dc23c2ca7e5a712a8c0fd23be6012792
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2017-12-14 21:45:28 +0200

    Update linux main

--- linux/main.cpp
+++ linux/main.cpp
@@ -20,32 +20,30 @@
 static BlynkTransportSocket _blynkTransport;
 BlynkSocket Blynk(_blynkTransport);
 
+static const char *auth, *serv;
+static uint16_t port;
+
 #include <BlynkWidgets.h>
 
 BLYNK_WRITE(V1)
 {
-  printf("Got a value: %s\n", param[0].asStr());
+    printf("Got a value: %s\n", param[0].asStr());
 }
-
 
 void setup()
 {
-
+    Blynk.begin(auth, serv, port);
 }
 
 void loop()
 {
-  Blynk.run();
+    Blynk.run();
 }
 
 
 int main(int argc, char* argv[])
 {
-    const char *auth, *serv;
-    uint16_t port;
     parse_options(argc, argv, auth, serv, port);
-
-    Blynk.begin(auth, serv, port);
 
     setup();
     while(true) {
