39b85b86062b3f483f73380727364678b0afdb61
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2018-04-02 22:51:04 +0300

    Boost ver

--- src/Blynk/BlynkConfig.h
+++ src/Blynk/BlynkConfig.h
@@ -25,7 +25,7 @@
  * Professional settings
  ***************************************************/
 // Library version.
-#define BLYNK_VERSION        "0.5.1"
+#define BLYNK_VERSION        "0.5.2"
 
 // Heartbeat period in seconds.
 #ifndef BLYNK_HEARTBEAT
