86adc2b98015eaef81e9f7ac62b5c8f6664e6619
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2018-05-14 13:01:12 +0300

    Use BLYNK_NEWLINE

--- src/Blynk/BlynkProtocol.h
+++ src/Blynk/BlynkProtocol.h
@@ -94,24 +94,24 @@ protected:
 #if defined(BLYNK_NO_FANCY_LOGO)
         BLYNK_LOG1(BLYNK_F("Blynk v" BLYNK_VERSION " on " BLYNK_INFO_DEVICE));
 #elif defined(BLYNK_FANCY_LOGO_3D)
-        BLYNK_LOG1(BLYNK_F("\n"
-            "   ____     ___                      __\n"
-            "  /\\  _`\\  /\\_ \\                    /\\ \\  _\n"
-            "  \\ \\ \\_\\ \\\\//\\ \\    __  __     ___ \\ \\ \\/ \\\n"
-            "   \\ \\  _ <  \\ \\ \\  /\\ \\/\\ \\  /' _ `\\\\ \\ , <\n"
-            "    \\ \\ \\_\\ \\ \\_\\ \\_\\ \\ \\_\\ \\ /\\ \\/\\ \\\\ \\ \\\\`\\\n"
-            "     \\ \\____/ /\\____\\\\/`____ \\\\ \\_\\ \\_\\\\ \\_\\\\_\\\n"
-            "      \\/___/  \\/____/ `/___/\\ \\\\/_/\\/_/ \\/_//_/\n"
-            "                         /\\___/\n"
-            "                         \\/__/   " BLYNK_VERSION " on " BLYNK_INFO_DEVICE "\n"
+        BLYNK_LOG1(BLYNK_F(BLYNK_NEWLINE
+            "   ____     ___                      __" BLYNK_NEWLINE
+            "  /\\  _`\\  /\\_ \\                    /\\ \\  _" BLYNK_NEWLINE
+            "  \\ \\ \\_\\ \\\\//\\ \\    __  __     ___ \\ \\ \\/ \\" BLYNK_NEWLINE
+            "   \\ \\  _ <  \\ \\ \\  /\\ \\/\\ \\  /' _ `\\\\ \\ , <" BLYNK_NEWLINE
+            "    \\ \\ \\_\\ \\ \\_\\ \\_\\ \\ \\_\\ \\ /\\ \\/\\ \\\\ \\ \\\\`\\" BLYNK_NEWLINE
+            "     \\ \\____/ /\\____\\\\/`____ \\\\ \\_\\ \\_\\\\ \\_\\\\_\\" BLYNK_NEWLINE
+            "      \\/___/  \\/____/ `/___/\\ \\\\/_/\\/_/ \\/_//_/" BLYNK_NEWLINE
+            "                         /\\___/" BLYNK_NEWLINE
+            "                         \\/__/   " BLYNK_VERSION " on " BLYNK_INFO_DEVICE BLYNK_NEWLINE
         ));
 #else
-        BLYNK_LOG1(BLYNK_F("\n"
-            "    ___  __          __\n"
-            "   / _ )/ /_ _____  / /__\n"
-            "  / _  / / // / _ \\/  '_/\n"
-            " /____/_/\\_, /_//_/_/\\_\\\n"
-            "        /___/ v" BLYNK_VERSION " on " BLYNK_INFO_DEVICE "\n"
+        BLYNK_LOG1(BLYNK_F(BLYNK_NEWLINE
+            "    ___  __          __" BLYNK_NEWLINE
+            "   / _ )/ /_ _____  / /__" BLYNK_NEWLINE
+            "  / _  / / // / _ \\/  '_/" BLYNK_NEWLINE
+            " /____/_/\\_, /_//_/_/\\_\\" BLYNK_NEWLINE
+            "        /___/ v" BLYNK_VERSION " on " BLYNK_INFO_DEVICE BLYNK_NEWLINE
         ));
 #endif
     }
