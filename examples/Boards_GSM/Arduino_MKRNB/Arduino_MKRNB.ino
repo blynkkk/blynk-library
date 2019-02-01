f3eda4525ed203cc9b325dd9c3b7911caa9fdd69
Riccardo Rizzo <37290960+Rocketct@users.noreply.github.com>
2019-02-01 14:47:57 +0100

    Added MKRNB 1500 support (#445)
    Added MKRNB support to Blynk library

---
+++ examples/Boards_GSM/Arduino_MKRNB/Arduino_MKRNB.ino
@@ -0,0 +1,47 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+ *************************************************************
+  This example shows how to use Arduino MKRNB 1500
+  to connect your project to Blynk.
+  Note: This requires MKRGSM library
+    from https://www.arduino.cc/en/Reference/MKRNB
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+#include <MKRNB.h>
+#include <BlynkSimpleMKNB.h>
+
+NBClient client;
+GPRS gprs;
+NB nbAccess;
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+// Your SIM credential
+// Leave empty, if missing pin
+char pin[]  = "";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  Blynk.begin(auth, nbAccess, gprs, client , pin);
+}
+void loop()
+{
+  Blynk.run();
+}
---
+++ src/Adapters/BlynkArduinoNB.h
@@ -0,0 +1,93 @@
+/**
+ * @file       BlynkArduinoNB.h
+ * @author     Riccardo Rizzo
+ * @license    This project is released under the MIT License (MIT)
+ * @date       Dec 2018
+ * @brief
+ *
+ */
+
+#ifndef BlynkArduinoNB_h
+#define BlynkArduinoNB_h
+
+#ifndef BLYNK_INFO_CONNECTION
+#define BLYNK_INFO_CONNECTION  "NBCom"
+#endif
+
+#ifndef BLYNK_HEARTBEAT
+#define BLYNK_HEARTBEAT 60
+#endif
+
+#ifndef BLYNK_TIMEOUT_MS
+#define BLYNK_TIMEOUT_MS 6000
+#endif
+
+#define BLYNK_SEND_ATOMIC
+
+#include <BlynkApiArduino.h>
+#include <Blynk/BlynkProtocol.h>
+#include <Adapters/BlynkArduinoClient.h>
+#include <MKRNB.h>
+
+class BlynkSIM
+    : public BlynkProtocol<BlynkArduinoClient>
+{
+    typedef BlynkProtocol<BlynkArduinoClient> Base;
+public:
+    BlynkSIM(BlynkArduinoClient& transp)
+        : Base(transp)
+    {}
+
+    bool connectNetwork(NB& modem, GPRS& gprs, const char* pin)
+    {
+        BLYNK_LOG1(BLYNK_F("Modem init..."));
+        if (!modem.begin(pin)) {
+           BLYNK_FATAL(BLYNK_F("Cannot init"));
+           return false;
+        }
+
+        BLYNK_LOG3(BLYNK_F("Connecting to "),"", BLYNK_F(" ..."));
+        if (!gprs.attachGPRS()) {
+            BLYNK_FATAL(BLYNK_F("Connect GPRS failed"));
+            return false;
+        }
+
+        BLYNK_LOG1(BLYNK_F("Connected to GPRS"));
+        return true;
+    }
+
+    void config(NB&        nb,
+                GPRS&       gprs,
+                NBClient&  nbclient,
+                const char* auth,
+                const char* domain = BLYNK_DEFAULT_DOMAIN,
+                uint16_t    port   = BLYNK_DEFAULT_PORT)
+    {
+        Base::begin(auth);
+        modem = &nb;
+        modemGPRS = &gprs;
+
+        this->conn.setClient(&nbclient);
+        this->conn.begin(domain, port);
+    }
+
+    void begin(const char* auth,
+               NB&        nb,
+               GPRS&       gprs,
+               NBClient&  nbclient,
+               const char* pin,
+               const char* domain = BLYNK_DEFAULT_DOMAIN,
+               uint16_t    port   = BLYNK_DEFAULT_PORT)
+    {
+        config(nb, gprs, nbclient, auth, domain, port);
+        connectNetwork(nb, gprs, pin);
+        while(this->connect() != true) {}
+    }
+
+private:
+    NB*       modem;
+    GPRS*     modemGPRS;
+    NBClient* nbclient;
+};
+
+#endif
