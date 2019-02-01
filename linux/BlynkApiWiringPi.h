4e259a8e96c8d612fa489deeae2840e0d7df3e79
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2019-02-01 21:28:56 +0200

    Update info message format

--- linux/BlynkApiWiringPi.h
+++ linux/BlynkApiWiringPi.h
@@ -30,15 +30,7 @@ BLYNK_FORCE_INLINE
 void BlynkApi<Proto>::sendInfo()
 {
     static const char profile[] BLYNK_PROGMEM = "blnkinf\0"
-#ifdef BOARD_FIRMWARE_VERSION
-        BLYNK_PARAM_KV("ver"    , BOARD_FIRMWARE_VERSION)
-        BLYNK_PARAM_KV("blynk"  , BLYNK_VERSION)
-#else
         BLYNK_PARAM_KV("ver"    , BLYNK_VERSION)
-#endif
-#ifdef BOARD_TEMPLATE_ID
-        BLYNK_PARAM_KV("tmpl"   , BOARD_TEMPLATE_ID)
-#endif
         BLYNK_PARAM_KV("h-beat" , BLYNK_TOSTRING(BLYNK_HEARTBEAT))
         BLYNK_PARAM_KV("buff-in", BLYNK_TOSTRING(BLYNK_MAX_READBYTES))
 #ifdef BLYNK_INFO_DEVICE
@@ -50,14 +42,20 @@ void BlynkApi<Proto>::sendInfo()
 #ifdef BLYNK_INFO_CONNECTION
         BLYNK_PARAM_KV("con"    , BLYNK_INFO_CONNECTION)
 #endif
+#ifdef BOARD_FIRMWARE_VERSION
+        BLYNK_PARAM_KV("fw"     , BOARD_FIRMWARE_VERSION)
+#endif
         BLYNK_PARAM_KV("build"  , __DATE__ " " __TIME__)
         "\0"
     ;
     const size_t profile_len = sizeof(profile)-8-2;
 
-    char mem_dyn[32];
+    char mem_dyn[64];
     BlynkParam profile_dyn(mem_dyn, 0, sizeof(mem_dyn));
     profile_dyn.add_key("conn", "Socket");
+#ifdef BOARD_TEMPLATE_ID
+    profile_dyn.add_key("tmpl", BOARD_TEMPLATE_ID);
+#endif
 
     static_cast<Proto*>(this)->sendCmd(BLYNK_CMD_INTERNAL, 0, profile+8, profile_len, profile_dyn.getBuffer(), profile_dyn.getLength());
     return;
