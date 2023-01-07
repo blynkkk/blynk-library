7933be5c4b251b9f870ac8c731691a2377ede7f5
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2023-01-07 19:50:50 +0200

    Improve MultiClient developer UX

---
+++ examples/More/ArduinoClientMulti/Arduino_MKR1400_GSM_ETH/Arduino_MKR1400_GSM_ETH.ino
@@ -0,0 +1,130 @@
+/*************************************************************
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+ *************************************************************
+  This sketch shows how to use multiple connectivity options
+  at the same time:
+    - Arduino MKR1400 GSM module provides GPRS
+    - Arduino MKR ETH shield provides Ethernet
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+//#define BLYNK_DEBUG
+
+/* Fill-in information from your Blynk Template here */
+//#define BLYNK_TEMPLATE_ID             "YourTemplateID"
+//#define BLYNK_TEMPLATE_NAME           "Device"
+//#define BLYNK_AUTH_TOKEN              ""
+
+#include <BlynkMultiClient.h>
+
+/*
+ * Ethernet
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+static EthernetClient   blynkEthernetClient;
+byte ETH_MAC[] =        { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+/*
+ * GSM modem
+ */
+
+#include <MKRGSM.h>
+
+static GSMModem         modem;
+static GPRS             gprs;
+static GSM              gsmAccess;
+static GSMClient        blynkGsmClient;
+static GSMSSLClient     blynkGsmClientSSL;
+
+const char SIM_PIN[]      = "";
+const char GPRS_APN[]     = "internet";
+const char GPRS_USER[]    = "";
+const char GPRS_PASS[]    = "";
+
+/*
+ * Main
+ */
+
+void setup()
+{
+  Serial.begin(115200);
+
+  delay(1000);
+
+  /*
+   * Connect Ethernet
+   */
+  if (Ethernet.begin(ETH_MAC, 5000L, 500L)) {
+    Serial.print("Ethernet IP: ");
+    Serial.println(Ethernet.localIP());
+  } else if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.");
+  } else if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  } else {
+    Serial.println("Ethernet: DHCP configuration failed.");
+  }
+
+  /*
+   * Connect GPRS
+   */
+  bool gsmConnected = false;
+  bool gprsConnected = false;
+
+  const uint32_t tstart = millis();
+  while (millis() - tstart < 20000) {
+    if (gsmAccess.begin(SIM_PIN) == GSM_READY) {
+      gsmConnected = true;
+      break;
+    }
+    delay(1000);
+  }
+  while (gsmConnected && millis() - tstart < 20000) {
+    if (gprs.attachGPRS(GPRS_APN, GPRS_USER, GPRS_PASS) == GPRS_READY) {
+      gprsConnected = true;
+      break;
+    }
+    delay(1000);
+  }
+
+  if (!gsmConnected) {
+    Serial.println("GSM not connected.");
+  } else if (!gprsConnected) {
+    Serial.println("GPRS not connected.");
+  } else {
+    Serial.print("GPRS IP: ");
+    Serial.println(gprs.getIPAddress());
+  }
+
+  /*
+   * Blynk
+   */
+
+  // Note: MKR ETH doesn't support SSL, use port 80 for Ethernet
+  Blynk.addClient("ETH", blynkEthernetClient, 80);
+  Blynk.addClient("GSM", blynkGsmClientSSL,   443);
+
+  Blynk.config(BLYNK_AUTH_TOKEN);
+}
+
+void loop() {
+  Blynk.run();
+  Ethernet.maintain();
+}
--- src/BlynkMultiClient.h
+++ src/BlynkMultiClient.h
@@ -26,26 +26,36 @@ template <typename T>
 class BlynkArduinoClientMulti
     : public BlynkArduinoClientGen<T>
 {
+    struct Entry {
+        String    name;
+        T*        client;
+        uint16_t  port;
+    };
 public:
     BlynkArduinoClientMulti() {}
 
-    void addClient(T& c) {
+    void addClient(const String& name, T& c, uint16_t port = BLYNK_DEFAULT_PORT_SSL) {
         if (_conn_qty == 0) {
             BlynkArduinoClientGen<T>::setClient(&c);
+            BlynkArduinoClientGen<T>::port = port;
         }
         if (_conn_qty < BLYNK_MAX_CLIENT_QTY) {
-            _conn_array[_conn_qty++] = &c;
+            Entry& e = _conn_array[_conn_qty++];
+            e.name =   name;
+            e.client = &c;
+            e.port =   port;
         }
     }
 
     bool connect() {
         for (int i = 0; i < _conn_qty; i++) {
-            BlynkArduinoClientGen<T>::setClient(_conn_array[i]);
+            BlynkArduinoClientGen<T>::setClient(_conn_array[i].client);
+            BlynkArduinoClientGen<T>::port = _conn_array[i].port;
             if (BlynkArduinoClientGen<T>::connect()) {
-                BLYNK_LOG3(BLYNK_F("Connection "), i, F(" OK"));
+                BLYNK_LOG2(_conn_array[i].name, BLYNK_F(" connection OK"));
                 return true;
             } else {
-                BLYNK_LOG3(BLYNK_F("Connection "), i, F(" failed"));
+                BLYNK_LOG2(_conn_array[i].name, BLYNK_F(" connection failed"));
             }
         }
         return false;
@@ -54,13 +64,13 @@ public:
     void disconnect() {
         BlynkArduinoClientGen<T>::isConn = false;
         for (int i = 0; i < _conn_qty; i++) {
-            _conn_array[i]->stop();
+            _conn_array[i].client->stop();
         }
     }
 
 private:
-    T*  _conn_array[BLYNK_MAX_CLIENT_QTY] = { NULL, };
-    int _conn_qty = 0;
+    Entry   _conn_array[BLYNK_MAX_CLIENT_QTY];
+    int     _conn_qty = 0;
 };
 
 template <typename Transport>
@@ -74,24 +84,22 @@ public:
     {}
 
     template<typename T>
-    void addClient(T& c) {
-        this->conn.addClient(c);
+    void addClient(const String& name, T& client, uint16_t port = BLYNK_DEFAULT_PORT_SSL) {
+        this->conn.addClient(name, client, port);
     }
 
     void config(const char* auth,
-                const char* domain = BLYNK_DEFAULT_DOMAIN,
-                uint16_t    port   = BLYNK_DEFAULT_PORT_SSL)
+                const char* domain = BLYNK_DEFAULT_DOMAIN)
     {
         Base::begin(auth);
-        this->conn.begin(domain, port);
+        this->conn.begin(domain, 0);
     }
 
     void config(const char* auth,
-                IPAddress   ip,
-                uint16_t    port = BLYNK_DEFAULT_PORT_SSL)
+                IPAddress   ip)
     {
         Base::begin(auth);
-        this->conn.begin(ip, port);
+        this->conn.begin(ip, 0);
     }
 
 };
