bf5ebb18eb0df842c921c00fd8f5e9035ec37428
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2023-06-27 13:43:41 +0300

    Add Edgent_NCP example

---
+++ examples/Blynk.Edgent/Edgent_NCP/Edgent_NCP.ino
@@ -0,0 +1,82 @@
+/*************************************************************
+  Blynk is a platform with iOS and Android apps to control
+  ESP32, Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build mobile and web interfaces for any
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: https://www.blynk.io
+    Sketch generator:           https://examples.blynk.cc
+    Blynk community:            https://community.blynk.cc
+    Follow us:                  https://www.fb.com/blynkapp
+                                https://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+ *************************************************************
+  Blynk.Edgent implements:
+  - Blynk.Inject - Dynamic WiFi credentials provisioning
+  - Blynk.Air    - Over The Air firmware updates
+  - Device state indication using a physical LED
+  - Credentials reset using a physical Button
+ *************************************************************/
+
+/* Fill in information from your Blynk Template here */
+/* Read more: https://bit.ly/BlynkInject */
+//#define BLYNK_TEMPLATE_ID           "TMPxxxxxx"
+//#define BLYNK_TEMPLATE_NAME         "Device"
+
+// Debug output
+#define BLYNK_PRINT                   Serial
+
+// Redefine NCP connection port settings, if needed
+//#define BLYNK_NCP_SERIAL            Serial1
+//#define BLYNK_NCP_BAUD              2000000
+
+#include <BlynkEdgentNCP.h>
+
+BlynkTimer timer;
+
+BLYNK_CONNECTED() {
+  BLYNK_LOG("Connected to Blynk 🙌");
+}
+
+BLYNK_DISCONNECTED() {
+  BLYNK_LOG("Blynk disconnected");
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  delay(3000);
+
+  BLYNK_LOG("Main firmware: %s", BLYNK_FIRMWARE_VERSION);
+
+  // Initialize the Blynk.NCP hardware
+  if (Blynk.initNCP()) {
+    String ver = Blynk.getNcpVersion();
+    BLYNK_LOG("Blynk.NCP firmware: %s", ver.c_str());
+  } else {
+    BLYNK_LOG("Cannot communicate to Blynk.NCP");
+    return;
+  }
+
+  // Print state changes
+  Blynk.onStateChange([]() {
+    BLYNK_LOG("State: %s", Blynk.getStateString());
+  });
+
+  // White labeling
+  //Blynk.setVendorPrefix("MyCompany");
+  //Blynk.setVendorServer("dashboard.mycompany.com");
+  // Product setup
+  Blynk.begin(BLYNK_TEMPLATE_ID, BLYNK_TEMPLATE_NAME);
+
+  // Publish some data periodically
+  timer.setInterval(1000, []() {
+    Blynk.virtualWrite(V0, millis());
+  });
+}
+
+void loop() {
+  timer.run();
+  Blynk.run();
+}
