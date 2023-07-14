b39fbaaa6c5db6d26ad837caa3c07518593f2105
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2023-07-14 21:10:17 +0300

    Deprecate Blynk.Edgent support for Arduino MKR and Nano boards in favour of Blynk.NCP

--- examples/Blynk.Edgent/Edgent_MKR1010/Edgent_MKR1010.ino
+++ examples/Blynk.Edgent/Edgent_MKR1010/Edgent_MKR1010.ino
@@ -1,50 +1,6 @@
-/*************************************************************
-  Blynk is a platform with iOS and Android apps to control
-  ESP32, Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build mobile and web interfaces for any
-  projects by simply dragging and dropping widgets.
-
-    Downloads, docs, tutorials: https://www.blynk.io
-    Sketch generator:           https://examples.blynk.cc
-    Blynk community:            https://community.blynk.cc
-    Follow us:                  https://www.fb.com/blynkapp
-                                https://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
- *************************************************************
-  Blynk.Edgent implements:
-  - Blynk.Inject - Dynamic WiFi credentials provisioning
-  - Blynk.Air    - Over The Air firmware updates
-  - Device state indication using a physical LED
-  - Credentials reset using a physical Button
-
-  Required libraries: WiFiNINA, ArduinoOTA, ArduinoHttpClient, Timer5
-
-  NOTE: Please also update the WiFi module firmware, if needed
- *************************************************************/
-
-/* Fill in information from your Blynk Template here */
-/* Read more: https://bit.ly/BlynkInject */
-//#define BLYNK_TEMPLATE_ID           "TMPxxxxxx"
-//#define BLYNK_TEMPLATE_NAME         "Device"
-
-#define BLYNK_FIRMWARE_VERSION        "0.1.0"
-
-#define BLYNK_PRINT Serial
-//#define BLYNK_DEBUG
-
-#define APP_DEBUG
-
-#include "BlynkEdgent.h"
-
-void setup()
-{
-  Serial.begin(115200);
-  delay(2000);
-
-  BlynkEdgent.begin();
-}
-
-void loop() {
-  BlynkEdgent.run();
-}
+/*
+ * Arduino 33 IoT and Arduino MKR1010 are no longer supported by Blynk.Edgent.
+ * Blynk now offers a better solution for Dual-MCU boards. Please use Blynk.NCP:
+ *  https://github.com/blynkkk/BlynkNcpExample
+ *
+ */
